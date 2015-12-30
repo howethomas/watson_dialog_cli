636951aac4fe1b61d975458dd970d434d805e4d3
Thomas Howe <ghostofbasho@gmail.com>
2015-12-30 11:14:59 -0500

    Added command line interface

--- app.js
+++ app.js
@@ -14,54 +14,82 @@
  * limitations under the License.
  */
 
-'use strict';
+'use strict'
 
-var express  = require('express'),
-  app        = express(),
-  path       = require('path'),
-  bluemix    = require('./config/bluemix'),
-  extend     = require('util')._extend,
-  watson     = require('watson-developer-cloud');
+var express = require('express')
+var app = express()
+var bluemix = require('./config/bluemix')
+var extend = require('util')._extend
+var watson = require('watson-developer-cloud')
 
 // Bootstrap application settings
-require('./config/express')(app);
+require('./config/express')(app)
 
 // if bluemix credentials exists, then override local
-var credentials =  extend({
-     "password": "XS4cXl6EHC0q",
-     "url": "https://gateway.watsonplatform.net/dialog/api",
-     "username": "4a53486a-4df2-49a5-a92a-91efad480192",
+var credentials = extend({
+  'password': 'XS4cXl6EHC0q',
+  'url': 'https://gateway.watsonplatform.net/dialog/api',
+  'username': '4a53486a-4df2-49a5-a92a-91efad480192',
   version: 'v1'
-}, bluemix.getServiceCreds('dialog')); // VCAP_SERVICES
+}, bluemix.getServiceCreds('dialog'))
 
-var dialog_id = process.env.DIALOG_ID || '<dialog-id>';
+var dialog_id = process.env.DIALOG_ID || '<dialog-id>'
 
 // Create the service wrapper
-var dialog = watson.dialog(credentials);
+var dialog = watson.dialog(credentials)
 
-app.post('/conversation', function(req, res, next) {
-  var params = extend({ dialog_id: dialog_id }, req.body);
-  dialog.conversation(params, function(err, results) {
-    if (err)
-      return next(err);
-    else
-      res.json({ dialog_id: dialog_id, conversation: results});
-  });
-});
+app.post('/conversation', function (req, res, next) {
+  var params = extend({ dialog_id: dialog_id }, req.body)
+  dialog.conversation(params, function (err, results) {
+    if (err) {
+      return next(err)
+    } else {
+      res.json({
+        dialog_id: dialog_id,
+        conversation: results
+      })
+    }
+  })
+})
 
-app.post('/profile', function(req, res, next) {
-  var params = extend({ dialog_id: dialog_id }, req.body);
-  dialog.getProfile(params, function(err, results) {
-    if (err)
-      return next(err);
-    else
-      res.json(results);
-  });
-});
+app.post('/profile', function (req, res, next) {
+  var params = extend({ dialog_id: dialog_id }, req.body)
+  dialog.getProfile(params, function (err, results) {
+    if (err) {
+      return next(err)
+    } else {
+      res.json(results)
+    }
+  })
+})
 
 // error-handler settings
-require('./config/error-handler')(app);
+require('./config/error-handler')(app)
 
-var port = process.env.VCAP_APP_PORT || 3000;
-app.listen(port);
-console.log('listening at:', port);
+var port = process.env.VCAP_APP_PORT || 3000
+app.listen(port)
+console.log('listening at:', port)
+
+// Also, let's hook into the stdin and get a command line
+// version working
+
+var conversation_id
+process.stdin.on('data', function (data) {
+  var params = {
+    dialog_id: dialog_id,
+    conversation_id: conversation_id,
+    input: data
+  }
+  dialog.conversation(params, function (err, results) {
+    if (err) {
+      console.log('Dialog error' + JSON.stringify(err))
+      return
+    } else {
+      conversation_id = results['conversation_id']
+      var arrayLength = results.response.length
+      for (var i = 0; i < arrayLength; i++) {
+        process.stdout.write(results.response[i] + '\n')
+      }
+    }
+  })
+})
